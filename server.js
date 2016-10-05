0e4cee873eba8db5ed4de4c7c45f9bf11c57ce7d
kirubakaran-rajendhran <Kirubakaran.Rajendhran@bestbuy.com>
2016-10-05 23:28:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/article-two', function (req, res) {
+  res.send('article two is served'));
 });
 
 app.get('/ui/style.css', function (req, res) {
